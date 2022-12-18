4f552f25ff685c16194a174608ed100133d3d128
Mark Lamourine <markllama@gmail.com>
2022-12-18 17:06:17 -0500

    pass env vars into script

--- build-image.sh
+++ build-image.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
-#echo "I am running!"
-#echo "=== BEGIN Environment ==="
-#env
-#echo "===  END  Environment ==="
+#
+# === Environment Variables ===
+# GITHUB_TOKEN
+
+# REGISTRY_NAME
+# REGISTRY_USERNAME
+# REGISTRY_PASSWORD
+
+# IMAGE_NAME
+# IMAGE_TAG
+# IMAGE_CACHED
+
+# CONDA_RC
+# CONDA_ENV
+# CONDA_PYTHON_VERSION
+
+function main() {
+    echo "I am running!"
+    echo "=== BEGIN Environment ==="
+    env
+    echo "===  END  Environment ==="
+}
+
+main $*
+
 echo "image-id=testvalue"
